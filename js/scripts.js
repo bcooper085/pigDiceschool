d1743ff1c65405a7b0062ebe8b304d85d56407e0
Brandon Cooper and Marc Larkin <brandon@>
2017-01-25 15:12:41 -0800

    add player constructor function and player objects, corrected running total

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,77 @@
 //Business Logic
-// var dice = function rollDice() {
-//   return Math.floor((Math.random() * 6) + 1);
-// }
-
  var turnScore = 0;
- var playerScore = 0;
+ var playerTotal1 = 0;
+ var playerTotal2 = 0;
+ var rollDice = 0;
 
-function Player(playerName, playerNumber, score) {
-  this.playerNumber = playerNumber;
-  this.playerName = playerName;
+function Player(score) {
+  // this.playerNumber = playerNumber;
+  // this.playerName = playerName;
   this.score = score;
 }
 
-// var player1 = new Player(Marc, 1, 0);
-// var player2 = new Player(Brandon, 2, 0);
+var player1 = new Player(0);
+var player2 = new Player(0);
 
-function currentRoll() {
-  var rollDice = Math.floor((Math.random() * 6) + 1);
+function rollDice1() {
+  rollDice = Math.floor((Math.random() * 6) + 1);
   if (rollDice > 1) {
-    return turnScore += rollDice;
+    return rollDice;
   } else {
-    return turnScore = 0;
+    return 0;
+  }
+}
+function addToTotal1() {
+  return player1.score += rollDice;
+}
+
+function rollDice2() {
+  rollDice = Math.floor((Math.random() * 6) + 1);
+  if (rollDice > 1) {
+    return rollDice;
+  } else {
+    return rollDice += 0;
+  }
+}
+function addToTotal2() {
+  return player2.score += rollDice;
+}
+
+function winner() {
+  if (playerTotal1 >= 100) {
+    alert("Player 1 wins");
+  } else if (playerTotal2 >=100) {
+    alert("Player 2 wins");
   }
 }
 
+
+
 //UI Logic
 $(function() {
-  $('.btn').click(function() {
-    event.preventDefault();
-    $('#result').text(currentRoll);
-  });
+  $('.btn1').click(function() {
+    $('.diceNow').text(rollDice1);
+    winner();
+  })
+  $('.switch1').click(function() {
+    $('.display-total-score-one').text(addToTotal1);
+    $('.btn1').hide();
+    $('.switch1').hide();
+    $('.btn2').show();
+    $('.switch2').show();
 
-  $('.switch').click(function() {
-    
-    playerScore += turnScore;
-  });
+  })
+  $('.btn2').click(function() {
+    $('.diceNow').text(rollDice2);
+    winner();
+  })
+  $('.switch2').click(function() {
+    $('.display-total-score-two').text(addToTotal2);
+    $('.btn2').hide();
+    $('.switch2').hide();
+    $('.btn1').show();
+    $('.switch1').show();
+
+
+  })
 });
--- readme.md
+++ readme.md
@@ -1,6 +1,8 @@
-Create a random number from 1 - 6
+Create a random number from 1 - 6 √
 
-Click a button to generate random number between 1 and 6
+Click a button to generate random number between 1 and 6 √
+
+take input from players to store names
 
 create two players and switch between players
 
@@ -9,3 +11,16 @@ store the value of the random number for each player each turn
 if a 1 is rolled games switches to other players and total for that turn is zero
 
 first player to 100 wins
+
+Add player names to respective columns
+
+if p1/p2 class = active
+  activate that player's side and deactivate the other
+
+
+
+if button "SWITCH" is clicked, add this turn score to total score
+
+
+give score box active class
+if score box has active, click switch to remove active and add to other box
