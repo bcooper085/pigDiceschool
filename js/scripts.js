0e87af4cb002995951d86a4dc2be22515d81d06c
Brandon Cooper and Marc Larkin <marc@>
2017-01-25 10:22:29 -0800

    add button to display roll score

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 //Business Logic
-function result() {
-  return Math.floor((Math.random() * 6) + 1);
-}
+// var dice = function rollDice() {
+//   return Math.floor((Math.random() * 6) + 1);
+// }
+
+ var turnScore = 0;
+ var playerScore = 0;
 
 function Player(playerName, playerNumber, score) {
   this.playerNumber = playerNumber;
@@ -9,14 +12,27 @@ function Player(playerName, playerNumber, score) {
   this.score = score;
 }
 
-var player1 = new Player(Marc, 1, 0);
-var player2 = new Player(Brandon, 2, 0);
+// var player1 = new Player(Marc, 1, 0);
+// var player2 = new Player(Brandon, 2, 0);
 
+function currentRoll() {
+  var rollDice = Math.floor((Math.random() * 6) + 1);
+  if (rollDice > 1) {
+    return turnScore += rollDice;
+  } else {
+    return turnScore = 0;
+  }
+}
 
 //UI Logic
 $(function() {
-  $('.btn').click(function () {
+  $('.btn').click(function() {
     event.preventDefault();
-    $('#result').text(result);
+    $('#result').text(currentRoll);
+  });
+
+  $('.switch').click(function() {
+    
+    playerScore += turnScore;
   });
 });
