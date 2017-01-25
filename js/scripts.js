718aa92c2dab342ee03bc62ce1132a9c8b0af1d6
Brandon Cooper and Marc Larkin <brandon@>
2017-01-25 09:10:17 -0800

    added switch sides button and divs to hold scores

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,16 @@ function result() {
   return Math.floor((Math.random() * 6) + 1);
 }
 
+function Player(playerName, playerNumber, score) {
+  this.playerNumber = playerNumber;
+  this.playerName = playerName;
+  this.score = score;
+}
+
+var player1 = new Player(Marc, 1, 0);
+var player2 = new Player(Brandon, 2, 0);
+
+
 //UI Logic
 $(function() {
   $('.btn').click(function () {
