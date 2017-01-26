9ffdc95d20d28ccb377b9b025ff32467f61f9383
Brandon Cooper and Marc Larkin <brandon@>
2017-01-26 09:02:31 -0800

    add username inputs and display names above respective sides and zero out the current turns score when players switch.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 //Business Logic
- var turnScore = 0;
- var playerTotal1 = 0;
- var playerTotal2 = 0;
- var rollDice = 0;
+var playerTotal1;
+var playerTotal2;
+var roll;
+var currentScore = 0;
+var currentPlayer = 1;
+
 
 function Player(score) {
-  // this.playerNumber = playerNumber;
   // this.playerName = playerName;
   this.score = score;
 }
@@ -13,44 +14,58 @@ function Player(score) {
 var player1 = new Player(0);
 var player2 = new Player(0);
 
-function rollDice1() {
-  rollDice = Math.floor((Math.random() * 6) + 1);
-  if (rollDice > 1) {
-    return rollDice;
+function rollDice() {
+  var roll = Math.floor((Math.random() * 6) + 1);
+  if (roll === 1) {
+    currentScore = 0;
+    switchPlayer();
   } else {
-    return 0;
+    currentScore += roll;
   }
-}
-function addToTotal1() {
-  return player1.score += rollDice;
+  return roll;
 }
 
-function rollDice2() {
-  rollDice = Math.floor((Math.random() * 6) + 1);
-  if (rollDice > 1) {
-    return rollDice;
+function switchPlayer() {
+  if (currentPlayer === 1) {
+    playerTotal1 += currentScore;
+    currentPlayer = 2;
   } else {
-    return rollDice += 0;
+    playerTotal2 += currentScore;
+    currentPlayer = 1;
   }
+  return playerTotal1;
+  return playerTotal2;
 }
+
+
+function addToTotal1() {
+  return player1.score += currentScore;
+}
+
 function addToTotal2() {
-  return player2.score += rollDice;
+  return player2.score += currentScore;
 }
 
 function winner() {
-  if (playerTotal1 >= 100) {
+  if (addToTotal1 >= 100) {
     alert("Player 1 wins");
-  } else if (playerTotal2 >=100) {
+  } else if (addToTotal2 >=100) {
     alert("Player 2 wins");
   }
 }
 
 
-
 //UI Logic
 $(function() {
+  $('.player1').submit(function() {
+    event.preventDefault();
+    $("#player-one-name").text($("#player-name-1").val());
+    $("#player-two-name").text($("#player-name-2").val());
+  });
+
   $('.btn1').click(function() {
-    $('.diceNow').text(rollDice1);
+    $('.diceNow').text(rollDice);
+    $('.currentTotal').text(currentScore);
     winner();
   })
   $('.switch1').click(function() {
@@ -59,10 +74,12 @@ $(function() {
     $('.switch1').hide();
     $('.btn2').show();
     $('.switch2').show();
+    currentScore = 0;
 
   })
   $('.btn2').click(function() {
-    $('.diceNow').text(rollDice2);
+    $('.diceNow').text(rollDice);
+    $('.currentTotal').text(currentScore);
     winner();
   })
   $('.switch2').click(function() {
@@ -71,7 +88,7 @@ $(function() {
     $('.switch2').hide();
     $('.btn1').show();
     $('.switch1').show();
-
+    currentScore = 0;
 
   })
 });
